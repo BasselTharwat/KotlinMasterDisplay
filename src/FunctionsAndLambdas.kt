4870033dceaf2aec8333de6dfb203aff5b8ef1e7
BasselTharwat <bassel.tharwat01@gmail.com>
2024-07-29 16:56:26 +0300

    lambda

--- src/FunctionsAndLambdas.kt
+++ src/FunctionsAndLambdas.kt
@@ -1,5 +1,17 @@
+fun applyToStrings(strings: List<String>, func: (String) -> String): List<String>{
 
+    val newStrings: List<String> = strings.map {
+        func(it)
+    }
+    return newStrings
+
+
+}
+fun ourFunc(string: String) = string.uppercase()
 
 fun main(args: Array<String>) {
+    val strings = listOf("h","a","ppp","y","is our motto")
+    println( applyToStrings(strings, ::ourFunc) ) //this will be the output [H, A, PPP, Y, IS OUR MOTTO]
+
 
 }
